d5fa043bb4badd28121c5f383362c22c69f9494a
jun <1494243221@qq.com>
2023-07-27 00:20:32 +0800

    #feat: 调整导航工具

--- docs/tool/toolsData.ts
+++ docs/tool/toolsData.ts
@@ -104,6 +104,130 @@ const toolsData = [
     ],
   },
   {
+    type: "常用工具",
+    top: 0,
+    show: true,
+    isSelect: false,
+    tools: [
+      {
+        title: "Clash",
+        icon: "https://raw.githubusercontent.com/Dreamacro/clash/master/docs/logo.png",
+        link: "https://github.com/Dreamacro/clash/releases/tag/premium",
+      },
+      {
+        title: "WindTerm",
+        icon: "",
+        link: "https://github.com/kingToolbox/WindTerm/releases",
+      },
+      {
+        title: "Docker",
+        icon: "https://www.docker.com/wp-content/uploads/2023/04/cropped-Docker-favicon-32x32.png",
+        link: "https://www.docker.com/",
+      },
+      {
+        title: "Apifox",
+        icon: "https://cdn.apifox.cn/logo/apifox-logo-256.png",
+        link: "https://apifox.com/",
+      },
+      {
+        title: "ToDesk",
+        icon: "https://www.todesk.com/dist/img/df0915d.png",
+        link: "https://www.todesk.com/",
+      },
+      {
+        title: "Figma",
+        icon: "https://static.figma.com/app/icon/1/favicon.ico",
+        link: "https://www.figma.com/",
+      },
+      {
+        title: "iconfont",
+        icon: "https://img.alicdn.com/imgextra/i4/O1CN01Z5paLz1O0zuCC7osS_!!6000000001644-55-tps-83-82.svg",
+        link: "https://www.iconfont.cn/",
+      },
+      {
+        title: "Regulex",
+        icon: "https://jex.im/favicon.ico",
+        link: "https://jex.im/regulex/#!flags=&re=%5E(a%7Cb)*%3F%24",
+      },
+      {
+        title: "Snipaste",
+        icon: "https://zh.snipaste.com/favicon.ico",
+        link: "https://zh.snipaste.com/",
+      },
+      {
+        title: "TinyPNG",
+        icon: "https://tinypng.com/images/favicon.ico",
+        link: "https://tinypng.com/",
+      },
+      {
+        title: "iloveimg",
+        icon: "https://www.iloveimg.com/img/favicons-img/favicon-16x16.png",
+        link: "https://www.iloveimg.com/zh-cn/compress-image",
+      },
+      {
+        title: "生成图片",
+        icon: "",
+        link: "https://ltaoo.github.io/tools/#/img-create",
+      },
+    ],
+  },
+  {
+    type: "组件库",
+    top: 0,
+    show: true,
+    isSelect: false,
+    tools: [
+      {
+        title: "Ant Design Vue",
+        icon: "https://www.antdv.com/assets/logo.1ef800a8.svg",
+        link: "https://www.antdv.com/docs/vue/introduce-cn/",
+      },
+      {
+        title: "Element Plus",
+        icon: "https://element-plus.org/images/element-plus-logo-small.svg",
+        link: "https://element-plus.org/zh-CN/",
+      },
+      {
+        title: "TDesign",
+        icon: "https://cdc.cdn-go.cn/tdc/latest/images/tdesign.svg",
+        link: "https://tdesign.tencent.com/",
+      },
+      {
+        title: "Arco Design",
+        icon: "https://unpkg.byted-static.com/latest/byted/arco-config/assets/favicon.ico",
+        link: "https://arco.design/",
+      },
+      {
+        title: "Chakra UI",
+        icon: "https://chakra-ui.com/favicon.png",
+        link: "https://chakra-ui.com/",
+      },
+      {
+        title: "vue-element-admin",
+        icon: "https://panjiachen.github.io/vue-element-admin-site/home.png",
+        link: "https://panjiachen.github.io/vue-element-admin-site/",
+      },
+    ],
+  },
+  {
+    type: "静态站点",
+    top: 0,
+    show: true,
+    isSelect: false,
+    tools: [
+      {
+        title: "VitePress",
+        icon: "https://avatars.githubusercontent.com/u/6128107?s=48&v=4",
+        link: "https://vitepress.dev/",
+      },
+      {
+        title: "dumi",
+        icon: "https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png",
+        link: "https://d.umijs.org/",
+      },
+    ],
+  },
+  {
     type: "文档协作",
     top: 0,
     show: true,
@@ -133,112 +257,6 @@ const toolsData = [
         title: "Typora",
         icon: "https://www.typoraio.cn/img/favicon-32.png",
         link: "https://www.typoraio.cn/",
-      },
-    ],
-  },
-  {
-    type: "常用工具",
-    top: 0,
-    show: true,
-    isSelect: false,
-    tools: [
-      {
-        title: "Clash",
-        icon: "https://raw.githubusercontent.com/Dreamacro/clash/master/docs/logo.png",
-        link: "https://github.com/Dreamacro/clash/releases/tag/premium",
-      },
-      {
-        title: "WindTerm",
-        icon: "",
-        link: "https://github.com/kingToolbox/WindTerm/releases",
-      },
-      {
-        title: "Docker",
-        icon: "https://www.docker.com/wp-content/uploads/2023/04/cropped-Docker-favicon-32x32.png",
-        link: "https://www.docker.com/",
-      },
-      {
-        title: "Apifox",
-        icon: "https://cdn.apifox.cn/logo/apifox-logo-256.png",
-        link: "https://apifox.com/",
-      },
-      {
-        title: "ToDesk",
-        icon: "https://www.todesk.com/dist/img/0a54c32.png",
-        link: "https://www.todesk.com/",
-      },
-      {
-        title: "Figma",
-        icon: "https://static.figma.com/app/icon/1/favicon.ico",
-        link: "https://www.figma.com/",
-      },
-      {
-        title: "iconfont",
-        icon: "https://img.alicdn.com/imgextra/i4/O1CN01Z5paLz1O0zuCC7osS_!!6000000001644-55-tps-83-82.svg",
-        link: "https://www.iconfont.cn/",
-      },
-      {
-        title: "Snipaste",
-        icon: "https://zh.snipaste.com/favicon.ico",
-        link: "https://zh.snipaste.com/",
-      },
-      {
-        title: "TinyPNG",
-        icon: "https://tinypng.com/images/favicon.ico",
-        link: "https://tinypng.com/",
-      },
-      {
-        title: "iloveimg",
-        icon: "https://www.iloveimg.com/img/favicons-img/favicon-16x16.png",
-        link: "https://www.iloveimg.com/zh-cn/compress-image",
-      },
-      {
-        title: "Regulex",
-        icon: "https://jex.im/favicon.ico",
-        link: "https://jex.im/regulex/#!flags=&re=%5E(a%7Cb)*%3F%24",
-      },
-      {
-        title: "生成图片",
-        icon: "",
-        link: "https://ltaoo.github.io/tools/#/img-create",
-      },
-    ],
-  },
-  {
-    type: "组件库",
-    top: 0,
-    show: true,
-    isSelect: false,
-    tools: [
-      {
-        title: "Ant Design Vue",
-        icon: "https://aliyuncdn.antdv.com/favicon.ico",
-        link: "https://www.antdv.com/docs/vue/introduce-cn/",
-      },
-      {
-        title: "Element Plus",
-        icon: "https://element-plus.org/images/element-plus-logo-small.svg",
-        link: "https://element-plus.org/zh-CN/",
-      },
-      {
-        title: "TDesign",
-        icon: "https://cdc.cdn-go.cn/tdc/latest/images/tdesign.svg",
-        link: "https://tdesign.tencent.com/",
-      },
-      {
-        title: "Arco Design",
-        icon: "https://unpkg.byted-static.com/latest/byted/arco-config/assets/favicon.ico",
-        link: "https://arco.design/",
-      },
-      {
-        title: "Chakra UI",
-        icon: "https://chakra-ui.com/favicon.png",
-        link: "https://chakra-ui.com/",
-      },
-      {
-        title: "vue-element-admin",
-        icon: "https://panjiachen.github.io/vue-element-admin-site/home.png",
-        link: "https://panjiachen.github.io/vue-element-admin-site/",
       },
     ],
   },
