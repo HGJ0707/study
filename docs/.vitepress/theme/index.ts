14c1129c7bb731e340bb740934066988a6e42e60
huguangjun <huguangjun@xxpie.com>
2023-07-28 18:01:28 +0800

    #feat: 新增二维码

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,14 @@
-import { h } from 'vue'
+import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import BackToTop from "./components/BackToTop/index.vue";
+import MobileQrCode from "./components/MobileQrCode/index.vue";
 
 export default {
   ...DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
       "aside-outline-before": () => h(BackToTop),
+      "nav-bar-content-after": () => h(MobileQrCode),
     });
   },
 };
