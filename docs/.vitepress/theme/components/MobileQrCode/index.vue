fef6c446faade002dffc91bc726756356aae4caf
huguangjun <huguangjun@xxpie.com>
2023-07-29 13:36:18 +0800

    #fix: 更改二维码弹窗样式

--- docs/.vitepress/theme/components/MobileQrCode/index.vue
+++ docs/.vitepress/theme/components/MobileQrCode/index.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import QRCode from "qrcode";
-import iconQrcode from '../../../../public/images/icons/iconQrcode.svg?raw';
+import iconQrcode from "../../../../public/images/icons/iconQrcode.svg?raw";
+import iconClose from "../../../../public/images/icons/iconClose.svg?raw";
 
 const codeUrl = ref(null);
 const isShowCode = ref(false);
 
-const createCodeUrl = async() => {
+const createCodeUrl = async () => {
   try {
     const url = await QRCode.toDataURL(window.location.href, {
-      width: 150,
-      margin: 0
-    })
+      width: 170,
+      margin: 5,
+    });
     codeUrl.value = url;
   } catch (error) {
     console.error(error);
   }
-    
 };
 
 const showCode = (isShow: boolean) => {
-  document.body.style.overflow = isShow ? "hidden" : "auto";
+  // document.body.style.overflow = isShow ? "hidden" : "auto";
   isShowCode.value = isShow;
 };
 
@@ -34,8 +34,12 @@ onMounted(() => {
     <div v-html="iconQrcode" class="code-icon"></div>
   </div>
   <teleport to="body">
-    <div v-show="isShowCode" class="code-mask" @click="showCode(false)">
+    <div v-show="isShowCode" class="code-mask">
       <div class="code-modal">
+        <div class="code-close" @click="showCode(false)">
+          <div v-html="iconClose" class="close-icon"></div>
+        </div>
+        <div class="code-text">手机扫码浏览</div>
         <img :src="codeUrl" class="code-img" alt="" />
       </div>
     </div>
@@ -71,24 +75,59 @@ onMounted(() => {
   display: flex;
   align-items: center;
   user-select: none;
-  cursor: pointer;
 }
 
 .code-modal {
-  border-radius: 8px;
-  background: #fff;
+  width: 240px;
+  border-radius: 4px;
+  background: var(--vp-c-bg);
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   position: relative;
-  padding: 30px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 2px 30px 0px;
+  padding: 50px 30px 30px;
 }
 
-@media (max-width: 800) {
+.code-text {
+  font-size: 16px;
+  font-weight: 600;
+  color: var(--vp-c-text-1);
+  margin-bottom: 30px;
+}
+
+.code-close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 24px;
+  height: 24px;
+  border-radius: 4px;
+  background: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+
+.code-close:hover {
+  background: var(--vp-c-bg-soft-down);
+}
+
+@media (max-width: 800px) {
   .nav-box {
     display: none;
   }
+}
+</style>
+
+<style>
+.code-modal .code-close svg {
+  fill: #000;
+  width: 14px;
+  height: 14px;
 }
 </style>
